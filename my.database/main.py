9ab658d8b4be251990ff57c373d11474da78a3ad
Barbara <149336996+BarbGuerrero@users.noreply.github.com>
2026-05-17 10:59:06 -0500

    using original zip instead of rebuilding

---
+++ my.database/main.py
@@ -0,0 +1,50 @@
+# interface (commandline handling)
+from database import Database
+
+def main():
+
+    db = Database()
+
+    # input loop
+    while True:
+        # prepare for input
+        try:
+            command = input().strip()
+        except EOFError:
+            break
+
+        if not command:
+            continue
+
+        # parse
+        parts = command.split(" ", 2)
+        action = parts[0]
+
+        # if SET
+        if action == "SET" and len(parts) == 3:
+            key = parts[1]
+            val = parts[2]
+
+            db.set(key, val)
+            print("OK", flush=True)
+
+        # if GET
+        elif action == "GET" and len(parts) == 2:
+            key = parts[1]
+
+            result = db.get(key)
+
+            if result is None:
+                print("", flush=True)
+            else:
+                print(result)
+
+        elif action == "EXIT":
+            print("bye bye", flush=True)
+            break
+
+        else:
+            print("ERROR", flush=True)
+
+if __name__ == "__main__":
+    main()
